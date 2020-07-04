d880a69b2c6b26d657493e99f24a45d4e9066642
Henry Widd <henryw374@gmail.com>
2020-07-04 16:02:11 +0100

    now getting JSC_LATE_PROVIDE_ERROR - from cyclic deps

--- dev/mod_js_joda.clj
+++ dev/mod_js_joda.clj
@@ -5,19 +5,34 @@
 (defn js-joda-files []
   (file-seq (io/file "jsjoda")))
 
+(defn ->package-name [path]
+  (let [path (if (string/ends-with? path ".js")
+               (subs path 0 (- (count path) (count ".js")))
+               path)]
+    (string/join "." (string/split path #"/"))))
+
 (defn copy-file [f]
+  (println f)
   (let [path (.getPath f)
-        package-parts (-> path
-                          (subs 0 (- (count path) (count ".js")))
-                          (string/split #"/"))
-        package-name (string/join "." package-parts)
-        contents (slurp path)
+        path-parts (butlast (string/split path #"/"))
+        package-name (->package-name path)
+        contents (line-seq (io/reader f))
         new-file (io/file (str "src/" path))]
     (io/make-parents new-file)
     (spit (.getPath new-file)
-      (str
+      (apply str
         "goog.declareModuleId('" package-name "');\n"
-        contents))
+        (map 
+          (fn [line]
+            (if-let [m (re-find #"import \{(.*)\} from '([.\/]+)/(.*)';" line)]
+              (let [[_ consts rel imp] m
+                    package-prefix (case rel 
+                                     "." (str (string/join "/" path-parts) "/") 
+                                     ".." (str (string/join "/" (butlast path-parts)) "/")
+                                     "../.." (str (string/join (butlast (butlast path-parts))) "/"))]
+                (str "const { " consts " } = goog.require('" (->package-name (str package-prefix imp)) "');\n"))
+              (str line "\n")))
+          contents)))
     ;(println "name ")
     ))
 
@@ -28,5 +43,6 @@
                  )]
     (when-not (.isDirectory f)
       (copy-file f)))
-  
+
+  (last (js-joda-files))
   )
