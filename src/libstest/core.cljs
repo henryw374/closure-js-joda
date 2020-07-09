eb76be089bfeaa5c400c5f645b05378875a414bb
Henry Widd <henryw374@gmail.com>
2020-07-09 11:53:26 +0100

    tidy

--- src/libstest/core.cljs
+++ src/libstest/core.cljs
@@ -1,10 +1,9 @@
 (ns libstest.core
-  (:require [libstest.jsjoda])
-  )
+  (:require [libstest.jsjoda]))
 
 (defn yay []
-  ;(.init libstest.jsjoda)
-  ; (.now libstest.jsjoda/Instant)
-  (.-MONDAY libstest.jsjoda/DayOfWeek)
+  
+   (.now libstest.jsjoda/Instant)
+  ;(.-MONDAY libstest.jsjoda/DayOfWeek)
   )
 
