92182207b5a9a820ccca1da7070f7b5d753090e9
Henry Widd <henryw374@gmail.com>
2020-08-09 08:02:46 +0100

    tz and locales working  - not tried advanced

--- src/cljs/java_time/addons.cljs
+++ src/cljs/java_time/addons.cljs
@@ -1,13 +1,36 @@
 (ns cljs.java-time.addons
   (:require [raw.jsjoda]))
 
-(def locale-props
-  #js{"DateTimeFormatterBuilder" raw.jsjoda/DateTimeFormatterBuilder
-      "DateTimeFormatter" raw.jsjoda/DateTimeFormatter})
+(def locale-fn
+  (memoize
+    (fn [f]
+      (let [in #js{"DateTimeFormatterBuilder" raw.jsjoda/DateTimeFormatterBuilder
+                   "DateTimeFormatter"        raw.jsjoda/DateTimeFormatter
+                   "ValueRange"               raw.jsjoda/ValueRange
+                   "ChronoField"              raw.jsjoda/ChronoField
+                   "ChronoUnit"               raw.jsjoda/ChronoUnit
+                   "IsoFields"                raw.jsjoda/IsoFields
+                   "DayOfWeek"                raw.jsjoda/DayOfWeek
+                   "_"                        #js{"assert"   #js {"requireNonNull"  raw.jsjoda/requireNonNull
+                                                                  "requireInstance" raw.jsjoda/requireInstance} ;raw.jsjoda/assert
+                                                  "MathUtil" raw.jsjoda/MathUtil
+                                                  }
+                   }
+            use (fn [f] (f in))]
+        (goog.object/set in "use" use)
+        (f in)
+        ;(set! raw.jsjoda/DateTimeFormatterBuilder (goog.object/get in "DateTimeFormatterBuilder"))
+        ;(set! raw.jsjoda/DateTimeFormatter (goog.object/get in "DateTimeFormatter"))
+        ))))
 
-(def tz-props
-  #js{"use" raw.jsjoda/bindUse
-      "ZoneId" raw.jsjoda/ZoneId
-      "ZoneRules" raw.jsjoda/ZoneRules
-      "ZoneRulesProvider" raw.jsjoda/ZoneRulesProvider})
+(defn tz-fn [f]
+  (let [in #js{"ZoneId"            raw.jsjoda/ZoneId
+               "ZoneRules"         raw.jsjoda/ZoneRules
+               "ZoneRulesProvider" raw.jsjoda/ZoneRulesProvider}
+        use (fn [f] (f in))]
+    (goog.object/set in "use" use)
+    (f in)
+    (set! raw.jsjoda/ZoneId (goog.object/get in "ZoneId"))
+    (set! raw.jsjoda/ZoneRules (goog.object/get in "ZoneRules"))
+    (set! raw.jsjoda/ZoneRulesProvider (goog.object/get in "ZoneRulesProvider"))))
 
--- src/libstest/core.cljs
+++ src/libstest/core.cljs
@@ -1,16 +1,33 @@
 (ns libstest.core
   (:require [raw.jsjoda]
             [addons.timezone.js-joda-timezone]
+            [addons.locale.en-us]
             [cljs.java-time.addons]))
 
+(defn add-en-us! []
+  (cljs.java-time.addons/locale-fn addons.locale.en-us/f)) ;
+
 (defn add-tz! []
-  (addons.timezone.js-joda-timezone/f cljs.java-time.addons/tz-props))
+  (cljs.java-time.addons/tz-fn addons.timezone.js-joda-timezone/f))
 
 
 (defn yay []
   (add-tz!)
   (.of raw.jsjoda/ZoneId "Europe/Berlin")
+  (add-en-us!)
   ;(.now raw.jsjoda/Instant)
   ;(.-MONDAY libstest.jsjoda/DayOfWeek)
   )
 
+(comment 
+  (add-en-us!)
+  (-> raw.jsjoda/DateTimeFormatter
+      (.ofPattern "dd/MM/yy")
+      (.withLocale
+        (some->
+          (goog.object/get (add-en-us!) "Locale")
+          (goog.object/get "US")))
+      (.format (.parse raw.jsjoda/LocalDate "2020-02-02")))
+  (.getAvailableZoneIds raw.jsjoda/ZoneId)
+  (.now raw.jsjoda/Instant)
+  )
